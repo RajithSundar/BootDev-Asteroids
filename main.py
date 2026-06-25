c4ee5cb24e3c25c88ea78cb43548779c37a61ee2
Rajith Sundar <rajithsundarpillai@gmail.com>
2026-06-25 16:32:21 +0000

    Add pygame window and event loop

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pygame
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from logger import log_state
 def main():
-    print("Hello from bootdev-asteriods!")
+    pygame.init()
+    print(f"Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
